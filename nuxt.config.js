d81a2b6ee938423318ab0615b300ee2a5223c273
wikyyuen <wikyyuen@gmail.com>
2021-12-23 15:59:35 +0800

    优化路径配置

--- nuxt.config.js
+++ nuxt.config.js
@@ -60,8 +60,19 @@ export default {
     //   }
     // }
   },
+  generate: {
+    // 自定义配置路由
+    router: {
+      mode: 'hash', // 使用 'hash' 主要是为了适配以相对路径打开的静态站点， 必须使用 'hash' 否则路由跳转不生效
+      // base: '/moli/',
+      base: './'
+    },
+  },
   router: {
-    base: './'
-  }
+    mode: 'hash', // 使用 'hash' 主要是为了适配以相对路径打开的静态站点， 必须使用 'hash' 否则路由跳转不生效
+    // base: '/moli/',
+    base: process.env.NODE_ENV === 'production' ? './' : '/',
+  },
+
 
 }
