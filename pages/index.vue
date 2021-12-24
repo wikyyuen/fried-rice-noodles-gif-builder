cc11e5623ff0ffee4d02bdddd028a56380bd12ea
wikyyuen <wikyyuen@gmail.com>
2021-12-24 19:26:57 +0800

    加入居中样式

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,28 @@
-
+<style>
+.div-center{
+  display: -webkit-box;
+  -webkit-box-pack: center;
+  -webkit-box-align: center;
+  -webkit-box-orient: vertical;
+  text-align: center;
+}
+</style>
 <template>
   <div>
     <el-container>
       <el-header>
-        <el-row >
-          <el-col ><span style="font-weight: bold" >炒粉GIF生成器</span></el-col>
+        <el-row>
+          <el-col><span style="font-weight: bold">炒粉GIF生成器</span></el-col>
         </el-row>
       </el-header>
       <el-main>
-        <el-row  >
-          <el-col :span="6">
-            <div>
-              <el-image
-                style="width: 240px; height: 220px"
-                :src="img_gif"                  
-              >
+        <el-row >
+          <el-col class="div-center">
+              <el-image style="width: 240px; height: 220px" class="div-center" :src="img_gif">
                 <div slot="error" class="image-slot">
                   <i class="el-icon-loading"></i>
                 </div>
               </el-image>
-            </div>
           </el-col>
         </el-row>
         <el-row :gutter="20" class="row-bg" justify="center">
@@ -62,9 +65,20 @@
           </el-col>
         </el-row>
       </el-main>
-      <el-footer>
+      <el-footer class="div-center">
         <el-row class="row-bg" justify="center">
-          <el-col><span>Created By Yuen，仅供学习</span></el-col>
+          <el-col>
+            <el-link
+              href="https://github.com/wikyyuen/fried-rice-noodles-gif-builder"
+              target="_blank"
+              type="primary"
+              >github</el-link
+            >
+          </el-col>
+        </el-row>
+
+        <el-row class="row-bg" justify="center">
+          <el-col><span>Created By Yuen，仅供学习</span> </el-col>
         </el-row>
       </el-footer>
     </el-container>
@@ -102,6 +116,7 @@ export default {
         fried_rice_noodles: { length: 21, name: "炒粉" },
         barbecue: { length: 20, name: "烧烤" },
       },
+      github_icon: require("~/assets/github-fill.svg"),
     };
   },
 
@@ -125,55 +140,6 @@ export default {
           }
         }
       );
-    },
-    mergePic2(images, style, input, new_images, func) {
-      if (images.length) {
-        let image = images.shift();
-        const canvas = document.createElement("canvas");
-        canvas.width = 240;
-        canvas.height = 220;
-
-        const ctx = canvas.getContext("2d");
-        let img = new Image();
-        img.src = image;
-        img.onload = () => {
-          ctx.drawImage(img, 0, 0, 240, 135);
-          let img2 = new Image();
-          img2.src = style.payicon;
-          img2.onload = () => {
-            ctx.drawImage(img2, 0, 135, 240, 85);
-            let img3 = new Image();
-            img3.src = style.juhuasuan;
-            img3.onload = () => {
-              ctx.drawImage(img3, 0, 0, 70, 30);
-              //添加文字
-              ctx.font = "bolder 24px Microsoft YaHei";
-              ctx.textAlign = "center";
-              ctx.textBaseline = "bottom";
-              ctx.fillStyle = "#000000";
-              ctx.fillText(
-                "落魄" + input.position + " 在线" + input.gif_name,
-                120,
-                172
-              );
-              ctx.font = "bolder 22px Microsoft YaHei";
-              ctx.fillStyle = "#000000";
-              ctx.textAlign = "right";
-              ctx.textBaseline = "top";
-              ctx.fillText(input.right_up_input, 235, 15);
-              ctx.strokeStyle = "red";
-              ctx.lineWidth = 5;
-              ctx.strokeRect(0, 0, 240, 220);
-              let base64 = canvas.toDataURL();
-              // new_images.push({ base: 1, item: image });
-              new_images.push(base64);
-              this.mergePic(images, style, input, new_images, func);
-            };
-          };
-        };
-      } else {
-        func(new_images);
-      }
     },
     mergePic(images, style, input, func) {
       let new_images = [];
